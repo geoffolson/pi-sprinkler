a619e643932cb9d7140ab523b4f21a8929b34f25
geoffolson <geoffrey.olson.jr@gmail.com>
2024-07-07 00:38:40 -0700

    add zod schemas to define api input

---
+++ common/src/zodObjects.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const IrrigationZone = z.object({
+  channel: z.number(),
+  name: z.string(),
+  precipitationRate: z.number().optional().default(2)
+});
+
+export const ToggleIrrigationZone = z.number();
+
+// add option to use precipitation instead of length of time
+export const IrrigationZoneDuration = z.object({
+  channel: z.number(),
+  length: z.number()
+})
+
+export const IrrigationSchedule = z.object({
+  schedule: z.number(),
+  channels: z.array(IrrigationZoneDuration)
+})
