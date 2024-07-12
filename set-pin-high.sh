124085d31e700d2a8d7dade1182bfcee9a8bfcea
geoffolson <geoffrey.olson.jr@gmail.com>
2024-07-11 19:04:57 -0700

    add curl request to test new endpoint

---
+++ set-pin-high.sh
@@ -0,0 +1,4 @@
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{ "out":1, "channel":2 }' \
+  http://localhost:3000/irrigation-pin
---
+++ set-pin-low.sh
@@ -0,0 +1,4 @@
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{ "out":0, "channel":2 }' \
+  http://localhost:3000/irrigation-pin
